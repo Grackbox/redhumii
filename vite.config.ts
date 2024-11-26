4e51db5135d575d60d72bdd79d1bb8d7aff8dd66
Grack <grackbox@gmail.com>
2024-11-26 17:16:45 +0500

    fix3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Videochat/",
+  base: "/redhumii/",
   // server: {
   //   host: "192.168.100.3",
   // },
